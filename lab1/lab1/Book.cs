50e03c63a070a2670ee2dc2a235a29fe6b1facd4
archilkarchava <archil1997@gmail.com>
2019-09-23 12:51:49 +0500

    first commit

---
+++ lab1/lab1/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    public class Book
+    {
+        // Auto-implemented readonly property:
+        public string Title { get; }
+        public string Author { get; }
+        public string Genre { get; }
+        public string OriginalLanguage { get; }
+        public int Pages { get; }
+        public DateTime PublicationDate { get; }
+        // public Book()
+        // {
+        //     Title = "unknown";
+        // }
+
+        // Constructor that takes one argument:
+        public Book(string title, string author, string genre, string original_language, int pages, DateTime publication_date)
+        {
+            Title = title;
+            Author = author;
+            OriginalLanguage = original_language;
+            Pages = pages;
+            PublicationDate = publication_date;
+        }
+
+        // Method that overrides the base class (System.Object) implementation.
+        public override string ToString()
+        {
+            return string.Format("Title: {0}\nAuthor: {1}\nPublished: {2}\nLanguage: {3}", Title, Author, PublicationDate.Year, OriginalLanguage);
+        }
+    }
+}
---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Ulysses", "James Joyce", "Modernist novel", "English", 730, new DateTime(1922, 2, 2));
+            Console.WriteLine(book1.ToString());
+        }
+    }
+}
